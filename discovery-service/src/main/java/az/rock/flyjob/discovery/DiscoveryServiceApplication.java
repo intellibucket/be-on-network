b6ed12a064baca17b136db14743b6e6571bcf323
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-03-20 22:41:43 +0400

    enjoy life

---
+++ discovery-service/src/main/java/az/rock/flyjob/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package az.rock.flyjob.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
