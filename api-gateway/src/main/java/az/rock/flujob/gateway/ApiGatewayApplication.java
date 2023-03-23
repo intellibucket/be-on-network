5adea8fe403593006c0fd58cd9b4a04b0dac7b76
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-03-23 17:01:07 +0400

    enjoy life

---
+++ api-gateway/src/main/java/az/rock/flujob/gateway/ApiGatewayApplication.java
@@ -0,0 +1,10 @@
+package az.rock.flujob.gateway;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+}
