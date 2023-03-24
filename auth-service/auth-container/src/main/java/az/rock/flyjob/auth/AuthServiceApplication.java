c46bd38ffc378bd326cb95ccfb8ca43b52432342
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-03-24 15:52:59 +0400

    enjoy life

--- auth-service/auth-container/src/main/java/az/rock/flyjob/auth/AuthServiceApplication.java
+++ auth-service/auth-container/src/main/java/az/rock/flyjob/auth/AuthServiceApplication.java
@@ -3,8 +3,12 @@ package az.rock.flyjob.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@PropertySource("classpath:application.yml")
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
