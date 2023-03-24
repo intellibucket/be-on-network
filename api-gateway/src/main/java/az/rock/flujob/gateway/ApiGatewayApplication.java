c186d47fb4f98a2e96e9346206a96a577febaaef
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-03-24 12:01:23 +0400

    enjoy life

--- api-gateway/src/main/java/az/rock/flujob/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/az/rock/flujob/gateway/ApiGatewayApplication.java
@@ -1,10 +1,14 @@
 package az.rock.flujob.gateway;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
