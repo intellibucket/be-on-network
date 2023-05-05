0435b04214067fbe67ae5999c39a5ad32befe9d3
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-05-05 19:18:56 +0400

    enjoy life

--- library/rock-domain/src/main/java/az/rock/lib/domain/SimpleAggregateRoot.java
+++ library/rock-domain/src/main/java/az/rock/lib/domain/SimpleAggregateRoot.java
@@ -1,6 +1,6 @@
 package az.rock.lib.domain;
 
-public class SimpleAggregateRoot <ID> extends RootID<ID> {
+public abstract class SimpleAggregateRoot <ID> extends RootID<ID> {
     public SimpleAggregateRoot(ID value) {
         super(value);
     }
--- library/rock-domain/src/main/java/az/rock/lib/valueObject/Money.java
+++ library/rock-domain/src/main/java/az/rock/lib/valueObject/Money.java
@@ -3,6 +3,7 @@ package az.rock.lib.valueObject;
 import az.rock.lib.annotation.ValueObject;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.Objects;
 
@@ -12,29 +13,45 @@ public class Money {
 
     public static final Money ZERO = new Money(BigDecimal.ZERO);
 
-    public Money(BigDecimal amount) {
-        this.amount = amount;
+    public Money(final BigDecimal amount) {
+        this.amount = Objects.requireNonNullElse(amount, BigDecimal.ZERO);
     }
 
     public boolean isGreaterThanZero() {
         return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
     }
 
+    public boolean isGreaterThanZeroOrEquals() {
+        return this.amount.compareTo(BigDecimal.ZERO) >= 0;
+    }
+
     public boolean isGreaterThan(Money money) {
         return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
     }
 
-    public Money add(Money money) {
-        return new Money(setScale(this.amount.add(money.getAmount())));
+
+    public boolean isZero() {
+        return this.amount.compareTo(BigDecimal.ZERO) == 0;
     }
 
-    public Money subtract(Money money) {
-        return new Money(setScale(this.amount.subtract(money.getAmount())));
+
+    public Money add(Money money) {
+        return new Money(this.setScale(this.amount.add((BigDecimal) money.amount)));
     }
+
 
     public Money multiply(int multiplier) {
         return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
     }
+
+    public Money subtract(Money money) {
+        return new Money(this.setScale(this.amount.subtract(money.getAmount())));
+    }
+
+    public Money divide(Money money) {
+        return new Money(this.setScale(this.amount.divide(money.getAmount(), 2, RoundingMode.HALF_DOWN)));
+    }
+
 
     public BigDecimal getAmount() {
         return amount;
@@ -54,6 +71,6 @@ public class Money {
     }
 
     private BigDecimal setScale(BigDecimal input) {
-        return input.setScale(2, RoundingMode.HALF_EVEN);
+        return input.setScale(2, RoundingMode.HALF_DOWN);
     }
 }
