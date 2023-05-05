d890d3d6de9d1944ee7ef66e612c5d84a19d529b
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-05-05 15:52:16 +0400

    enjoy life

--- auth-service/auth-data-access/src/main/java/az/rock/flyjob/auth/dataAccess/DataAccessBeanConfig.java
+++ auth-service/auth-data-access/src/main/java/az/rock/flyjob/auth/dataAccess/DataAccessBeanConfig.java
@@ -1,10 +1,8 @@
 package az.rock.flyjob.auth.dataAccess;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
 public class DataAccessBeanConfig {
 
 }
