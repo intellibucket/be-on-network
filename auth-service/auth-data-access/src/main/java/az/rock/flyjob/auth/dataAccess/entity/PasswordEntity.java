59b56a94c488bc73e3961cb6a5d10a7ff6370848
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-05-09 17:23:36 +0400

    enjoy life

--- auth-service/auth-data-access/src/main/java/az/rock/flyjob/auth/dataAccess/entity/PasswordEntity.java
+++ auth-service/auth-data-access/src/main/java/az/rock/flyjob/auth/dataAccess/entity/PasswordEntity.java
@@ -1,7 +1,10 @@
 package az.rock.flyjob.auth.dataAccess.entity;
 
 import az.rock.lib.domain.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,7 +22,10 @@ public class PasswordEntity extends BaseEntity {
     @OneToOne
     private UserEntity userEntity;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "salt", nullable = false)
+    private String salt;
+
+    @Column(name = "hash", nullable = false)
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{8,}$", message = "Invalid password")
-    private String password;
+    private String hash;
 }
--- auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/ports/input/service/concretes/PasswordDomainPresentationService.java
+++ auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/ports/input/service/concretes/PasswordDomainPresentationService.java
@@ -1,10 +1,8 @@
 package az.rock.auth.domain.presentation.ports.input.service.concretes;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-@Slf4j
 @Validated
 @Service
 public class PasswordDomainPresentationService {
