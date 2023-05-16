937cc16b4ed6b2af0d3c6f6ba445a65d9f39c01f
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-05-16 16:58:13 +0400

    enjoy life

--- auth-service/auth-domain/auth-domain-core/src/main/java/az/rock/flyjob/auth/root/PhoneNumberRoot.java
+++ auth-service/auth-domain/auth-domain-core/src/main/java/az/rock/flyjob/auth/root/PhoneNumberRoot.java
@@ -1,9 +1,192 @@
 package az.rock.flyjob.auth.root;
 
 import az.rock.lib.domain.AggregateRoot;
-import az.rock.lib.domain.id.GeoPositionID;
 import az.rock.lib.domain.id.PhoneNumberID;
+import az.rock.lib.valueObject.DataStatus;
+import az.rock.lib.valueObject.PhoneNumberType;
+import az.rock.lib.valueObject.ProcessStatus;
+
+import java.math.BigInteger;
+import java.time.ZonedDateTime;
 
 public class PhoneNumberRoot extends AggregateRoot<PhoneNumberID> {
+    private final UserRoot user;
 
+    private final String countryCode;
+
+    private final String phoneNumber;
+
+    private final PhoneNumberType type;
+
+    private final Boolean isEnableSmsNotification;
+
+    private final Boolean isEnableWhatsappNotification;
+
+    private final Boolean isPrimary;
+
+    private final Boolean isVerified;
+
+    private final String verificationCode;
+
+    private final ZonedDateTime verificationCodeExpireDate;
+
+    private final ZonedDateTime verificationCodeSendDate;
+
+    private final BigInteger verificationCodeSendCount;
+
+    public PhoneNumberRoot(PhoneNumberID phoneNumberID,
+                           Long version,
+                           ProcessStatus processStatus,
+                           DataStatus dataStatus,
+                           ZonedDateTime createdDate,
+                           ZonedDateTime modificationDate,
+                           UserRoot user,
+                           String countryCode,
+                           String phoneNumber,
+                           PhoneNumberType type,
+                           Boolean isEnableSmsNotification,
+                           Boolean isEnableWhatsappNotification,
+                           Boolean isPrimary,
+                           Boolean isVerified,
+                           String verificationCode,
+                           ZonedDateTime verificationCodeExpireDate,
+                           ZonedDateTime verificationCodeSendDate,
+                           BigInteger verificationCodeSendCount) {
+        super(phoneNumberID, version, processStatus, dataStatus, createdDate, modificationDate);
+        this.user = user;
+        this.countryCode = countryCode;
+        this.phoneNumber = phoneNumber;
+        this.type = type;
+        this.isEnableSmsNotification = isEnableSmsNotification;
+        this.isEnableWhatsappNotification = isEnableWhatsappNotification;
+        this.isPrimary = isPrimary;
+        this.isVerified = isVerified;
+        this.verificationCode = verificationCode;
+        this.verificationCodeExpireDate = verificationCodeExpireDate;
+        this.verificationCodeSendDate = verificationCodeSendDate;
+        this.verificationCodeSendCount = verificationCodeSendCount;
+    }
+
+
+    public static final class Builder {
+        private UserRoot user;
+        private String countryCode;
+        private String phoneNumber;
+        private PhoneNumberType type;
+        private Boolean isEnableSmsNotification;
+        private Boolean isEnableWhatsappNotification;
+        private Boolean isPrimary;
+        private Boolean isVerified;
+        private String verificationCode;
+        private ZonedDateTime verificationCodeExpireDate;
+        private ZonedDateTime verificationCodeSendDate;
+        private BigInteger verificationCodeSendCount;
+        private Long version;
+        private ProcessStatus processStatus;
+        private DataStatus dataStatus;
+        private ZonedDateTime createdDate;
+        private ZonedDateTime modificationDate;
+        private PhoneNumberID phoneNumberID;
+
+        private Builder() {
+        }
+
+        public static Builder aPhoneNumberRoot() {
+            return new Builder();
+        }
+
+        public Builder user(UserRoot user) {
+            this.user = user;
+            return this;
+        }
+
+        public Builder countryCode(String countryCode) {
+            this.countryCode = countryCode;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder type(PhoneNumberType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder isEnableSmsNotification(Boolean isEnableSmsNotification) {
+            this.isEnableSmsNotification = isEnableSmsNotification;
+            return this;
+        }
+
+        public Builder isEnableWhatsappNotification(Boolean isEnableWhatsappNotification) {
+            this.isEnableWhatsappNotification = isEnableWhatsappNotification;
+            return this;
+        }
+
+        public Builder isPrimary(Boolean isPrimary) {
+            this.isPrimary = isPrimary;
+            return this;
+        }
+
+        public Builder isVerified(Boolean isVerified) {
+            this.isVerified = isVerified;
+            return this;
+        }
+
+        public Builder verificationCode(String verificationCode) {
+            this.verificationCode = verificationCode;
+            return this;
+        }
+
+        public Builder verificationCodeExpireDate(ZonedDateTime verificationCodeExpireDate) {
+            this.verificationCodeExpireDate = verificationCodeExpireDate;
+            return this;
+        }
+
+        public Builder verificationCodeSendDate(ZonedDateTime verificationCodeSendDate) {
+            this.verificationCodeSendDate = verificationCodeSendDate;
+            return this;
+        }
+
+        public Builder verificationCodeSendCount(BigInteger verificationCodeSendCount) {
+            this.verificationCodeSendCount = verificationCodeSendCount;
+            return this;
+        }
+
+        public Builder version(Long version) {
+            this.version = version;
+            return this;
+        }
+
+        public Builder processStatus(ProcessStatus processStatus) {
+            this.processStatus = processStatus;
+            return this;
+        }
+
+        public Builder dataStatus(DataStatus dataStatus) {
+            this.dataStatus = dataStatus;
+            return this;
+        }
+
+        public Builder createdDate(ZonedDateTime createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public Builder modificationDate(ZonedDateTime modificationDate) {
+            this.modificationDate = modificationDate;
+            return this;
+        }
+
+        public Builder uuid(PhoneNumberID phoneNumberID) {
+            this.phoneNumberID = phoneNumberID;
+            return this;
+        }
+
+        public PhoneNumberRoot build() {
+            return  new PhoneNumberRoot(null, version, processStatus, dataStatus, createdDate, modificationDate, user, countryCode, phoneNumber, type, isEnableSmsNotification, isEnableWhatsappNotification, isPrimary, isVerified, verificationCode, verificationCodeExpireDate, verificationCodeSendDate, verificationCodeSendCount);
+        }
+    }
 }
--- auth-service/auth-domain/auth-domain-core/src/main/java/az/rock/flyjob/auth/root/UserSettingsRoot.java
+++ auth-service/auth-domain/auth-domain-core/src/main/java/az/rock/flyjob/auth/root/UserSettingsRoot.java
@@ -2,7 +2,252 @@ package az.rock.flyjob.auth.root;
 
 import az.rock.lib.domain.AggregateRoot;
 import az.rock.lib.domain.id.UserSettingsID;
+import az.rock.lib.valueObject.DataStatus;
+import az.rock.lib.valueObject.Language;
+import az.rock.lib.valueObject.ProcessStatus;
+
+import java.time.ZonedDateTime;
 
 public class UserSettingsRoot extends AggregateRoot<UserSettingsID> {
+    private final UserRoot user;
 
+    private final Language language;
+
+    private final Boolean isActiveDarkMode;
+
+    private final Boolean isActiveEmailNotification;
+
+    private final Boolean isActiveSmsNotification;
+
+    private final Boolean isActivePushNotification;
+
+    private final Boolean isActiveTwoFactorAuthentication;
+
+    private final Boolean isVisibleLocation;
+
+    private final Boolean isVisibleOnlineStatus;
+
+    private final Boolean isVisibleLastSeen;
+
+    private final Boolean isVisibleProfilePicture;
+
+    private final Boolean isVisibleEmail;
+
+    private final Boolean isVisibleResume;
+
+    public UserSettingsRoot(UserSettingsID userSettingsID,
+                            Long version,
+                            ProcessStatus processStatus,
+                            DataStatus dataStatus,
+                            ZonedDateTime createdDate,
+                            ZonedDateTime modificationDate,
+                            UserRoot user,
+                            Language language,
+                            Boolean isActiveDarkMode,
+                            Boolean isActiveEmailNotification,
+                            Boolean isActiveSmsNotification,
+                            Boolean isActivePushNotification,
+                            Boolean isActiveTwoFactorAuthentication,
+                            Boolean isVisibleLocation,
+                            Boolean isVisibleOnlineStatus,
+                            Boolean isVisibleLastSeen,
+                            Boolean isVisibleProfilePicture,
+                            Boolean isVisibleEmail,
+                            Boolean isVisibleResume) {
+        super(userSettingsID, version, processStatus, dataStatus, createdDate, modificationDate);
+        this.user = user;
+        this.language = language;
+        this.isActiveDarkMode = isActiveDarkMode;
+        this.isActiveEmailNotification = isActiveEmailNotification;
+        this.isActiveSmsNotification = isActiveSmsNotification;
+        this.isActivePushNotification = isActivePushNotification;
+        this.isActiveTwoFactorAuthentication = isActiveTwoFactorAuthentication;
+        this.isVisibleLocation = isVisibleLocation;
+        this.isVisibleOnlineStatus = isVisibleOnlineStatus;
+        this.isVisibleLastSeen = isVisibleLastSeen;
+        this.isVisibleProfilePicture = isVisibleProfilePicture;
+        this.isVisibleEmail = isVisibleEmail;
+        this.isVisibleResume = isVisibleResume;
+    }
+
+
+    public UserRoot getUser() {
+        return user;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public Boolean getActiveDarkMode() {
+        return isActiveDarkMode;
+    }
+
+    public Boolean getActiveEmailNotification() {
+        return isActiveEmailNotification;
+    }
+
+    public Boolean getActiveSmsNotification() {
+        return isActiveSmsNotification;
+    }
+
+    public Boolean getActivePushNotification() {
+        return isActivePushNotification;
+    }
+
+    public Boolean getActiveTwoFactorAuthentication() {
+        return isActiveTwoFactorAuthentication;
+    }
+
+    public Boolean getVisibleLocation() {
+        return isVisibleLocation;
+    }
+
+    public Boolean getVisibleOnlineStatus() {
+        return isVisibleOnlineStatus;
+    }
+
+    public Boolean getVisibleLastSeen() {
+        return isVisibleLastSeen;
+    }
+
+    public Boolean getVisibleProfilePicture() {
+        return isVisibleProfilePicture;
+    }
+
+    public Boolean getVisibleEmail() {
+        return isVisibleEmail;
+    }
+
+    public Boolean getVisibleResume() {
+        return isVisibleResume;
+    }
+
+    public static final class Builder {
+        private UserRoot user;
+        private Language language;
+        private Boolean isActiveDarkMode;
+        private Boolean isActiveEmailNotification;
+        private Boolean isActiveSmsNotification;
+        private Boolean isActivePushNotification;
+        private Boolean isActiveTwoFactorAuthentication;
+        private Boolean isVisibleLocation;
+        private Boolean isVisibleOnlineStatus;
+        private Boolean isVisibleLastSeen;
+        private Boolean isVisibleProfilePicture;
+        private Boolean isVisibleEmail;
+        private Boolean isVisibleResume;
+        private Long version;
+        private ProcessStatus processStatus;
+        private DataStatus dataStatus;
+        private ZonedDateTime createdDate;
+        private ZonedDateTime modificationDate;
+        private UserSettingsID userSettingsID;
+
+        private Builder() {
+        }
+
+        public static Builder anUserSettingsRoot() {
+            return new Builder();
+        }
+
+        public Builder user(UserRoot user) {
+            this.user = user;
+            return this;
+        }
+
+        public Builder language(Language language) {
+            this.language = language;
+            return this;
+        }
+
+        public Builder isActiveDarkMode(Boolean isActiveDarkMode) {
+            this.isActiveDarkMode = isActiveDarkMode;
+            return this;
+        }
+
+        public Builder isActiveEmailNotification(Boolean isActiveEmailNotification) {
+            this.isActiveEmailNotification = isActiveEmailNotification;
+            return this;
+        }
+
+        public Builder isActiveSmsNotification(Boolean isActiveSmsNotification) {
+            this.isActiveSmsNotification = isActiveSmsNotification;
+            return this;
+        }
+
+        public Builder isActivePushNotification(Boolean isActivePushNotification) {
+            this.isActivePushNotification = isActivePushNotification;
+            return this;
+        }
+
+        public Builder isActiveTwoFactorAuthentication(Boolean isActiveTwoFactorAuthentication) {
+            this.isActiveTwoFactorAuthentication = isActiveTwoFactorAuthentication;
+            return this;
+        }
+
+        public Builder isVisibleLocation(Boolean isVisibleLocation) {
+            this.isVisibleLocation = isVisibleLocation;
+            return this;
+        }
+
+        public Builder isVisibleOnlineStatus(Boolean isVisibleOnlineStatus) {
+            this.isVisibleOnlineStatus = isVisibleOnlineStatus;
+            return this;
+        }
+
+        public Builder isVisibleLastSeen(Boolean isVisibleLastSeen) {
+            this.isVisibleLastSeen = isVisibleLastSeen;
+            return this;
+        }
+
+        public Builder isVisibleProfilePicture(Boolean isVisibleProfilePicture) {
+            this.isVisibleProfilePicture = isVisibleProfilePicture;
+            return this;
+        }
+
+        public Builder isVisibleEmail(Boolean isVisibleEmail) {
+            this.isVisibleEmail = isVisibleEmail;
+            return this;
+        }
+
+        public Builder isVisibleResume(Boolean isVisibleResume) {
+            this.isVisibleResume = isVisibleResume;
+            return this;
+        }
+
+        public Builder version(Long version) {
+            this.version = version;
+            return this;
+        }
+
+        public Builder processStatus(ProcessStatus processStatus) {
+            this.processStatus = processStatus;
+            return this;
+        }
+
+        public Builder dataStatus(DataStatus dataStatus) {
+            this.dataStatus = dataStatus;
+            return this;
+        }
+
+        public Builder createdDate(ZonedDateTime createdDate) {
+            this.createdDate = createdDate;
+            return this;
+        }
+
+        public Builder modificationDate(ZonedDateTime modificationDate) {
+            this.modificationDate = modificationDate;
+            return this;
+        }
+
+        public Builder uuid(UserSettingsID userSettingsID) {
+            this.userSettingsID = userSettingsID;
+            return this;
+        }
+
+        public UserSettingsRoot build() {
+            return new UserSettingsRoot(null, version, processStatus, dataStatus, createdDate, modificationDate, user, language, isActiveDarkMode, isActiveEmailNotification, isActiveSmsNotification, isActivePushNotification, isActiveTwoFactorAuthentication, isVisibleLocation, isVisibleOnlineStatus, isVisibleLastSeen, isVisibleProfilePicture, isVisibleEmail, isVisibleResume);
+        }
+    }
 }
