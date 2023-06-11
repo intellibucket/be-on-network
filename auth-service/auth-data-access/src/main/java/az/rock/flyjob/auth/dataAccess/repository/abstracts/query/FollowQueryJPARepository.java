0114b076ebe38ce74440cbfa1b23ebdac4c78519
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-06-11 22:56:03 +0400

    enjoy life

--- auth-service/auth-data-access/src/main/java/az/rock/flyjob/auth/dataAccess/repository/abstracts/query/FollowQueryJPARepository.java
+++ auth-service/auth-data-access/src/main/java/az/rock/flyjob/auth/dataAccess/repository/abstracts/query/FollowQueryJPARepository.java
@@ -1,12 +1,15 @@
 package az.rock.flyjob.auth.dataAccess.repository.abstracts.query;
 
 import az.rock.flyjob.auth.dataAccess.entity.network.FollowRelationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface FollowQueryJPARepository {
+@Repository
+public interface FollowQueryJPARepository extends JpaRepository<FollowRelationEntity,UUID> {
     @Query("select f from FollowRelationEntity f where f.followedUserId = ?1" +
             "and f.followStatus = 'ACCEPTED'")
     List<FollowRelationEntity> findAllMyFollowers(UUID absoluteID);
