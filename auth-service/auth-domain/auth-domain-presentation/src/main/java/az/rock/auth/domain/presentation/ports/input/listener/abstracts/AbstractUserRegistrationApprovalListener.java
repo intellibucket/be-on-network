357a1fc5b4f0cf7bd698051bc6f2710c7b66a301
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-06-25 22:41:18 +0400

    enjoy life

---
+++ auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/ports/input/listener/abstracts/AbstractUserRegistrationApprovalListener.java
@@ -0,0 +1,4 @@
+package az.rock.auth.domain.presentation.ports.input.listener.abstracts;
+
+public interface AbstractUserRegistrationApprovalListener {
+}
---
+++ auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/ports/input/listener/concretes/UserRegistrationApprovalListener.java
@@ -0,0 +1,8 @@
+package az.rock.auth.domain.presentation.ports.input.listener.concretes;
+
+import az.rock.auth.domain.presentation.ports.input.listener.abstracts.AbstractUserRegistrationApprovalListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRegistrationApprovalListener implements AbstractUserRegistrationApprovalListener {
+}
