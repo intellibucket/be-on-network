4f01a4c61c125638a690cb6fb378a279c5cd78bc
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-07-05 11:54:03 +0400

    Updated all kafka config

--- auth-service/auth-messaging/src/main/java/az/rock/flyjob/auth/messaging/listener/KafkaConsumer.java
+++ auth-service/auth-messaging/src/main/java/az/rock/flyjob/auth/messaging/listener/KafkaConsumer.java
@@ -4,4 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public interface KafkaConsumer <S>{
     <P extends S> P consume(JsonNode record);
+
+    void listen(JsonNode record);
 }
--- auth-service/auth-messaging/src/main/java/az/rock/flyjob/auth/messaging/listener/UserRegistrationApprovalConsumer.java
+++ auth-service/auth-messaging/src/main/java/az/rock/flyjob/auth/messaging/listener/UserRegistrationApprovalConsumer.java
@@ -2,13 +2,27 @@ package az.rock.flyjob.auth.messaging.listener;
 
 import az.rock.lib.valueObject.SagaRoot;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import model.auth.registration.CompanyRegistrationPayload;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRegistrationApprovalConsumer implements KafkaConsumer<SagaRoot<CompanyRegistrationPayload>>{
+    private final ObjectMapper objectMapper;
+
+    public UserRegistrationApprovalConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public <P extends SagaRoot<CompanyRegistrationPayload>> P consume(JsonNode record) {
         return null;
+    }
+
+    @Override
+    @KafkaListener(topics = "auth.user-registration.approval", groupId = "auth.userRegistration.approval")
+    public void listen(JsonNode record) {
+        var payload = this.consume(record);
     }
 }
