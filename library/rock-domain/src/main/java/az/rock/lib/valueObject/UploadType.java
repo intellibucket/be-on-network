7805b7ba6afb98278ba56b9dd4a0843da897a640
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-07-08 12:03:57 +0400

    Update ProfilePictureCommandDomainPresentation.java

--- library/rock-domain/src/main/java/az/rock/lib/valueObject/UploadType.java
+++ library/rock-domain/src/main/java/az/rock/lib/valueObject/UploadType.java
@@ -1,5 +1,38 @@
 package az.rock.lib.valueObject;
 
 public enum UploadType {
-    PICTURE, VIDEO, DOCUMENT, NONE;
+    PROFILE_PICTURE(new String[]{"image/jpeg", "image/png", "image/gif", "image/jpg"}){
+        @Override
+        public String getUploadPath() {
+            return "/image/profile/";
+        }
+    }, COVER_PICTURE(new String[]{"image/jpeg"}){
+        @Override
+        String getUploadPath() {
+            return "/image/cover/";
+        }
+    }, VIDEO(new String[]{"video/mp4"}){
+        @Override
+        String getUploadPath() {
+            return "/video/";
+        }
+    }, DOCUMENT(new String[]{"application/pdf"}){
+        @Override
+        String getUploadPath() {
+            return "/document/";
+        }
+    }, NONE(new String[]{}){
+        @Override
+        public String getUploadPath() {
+            return "/none/";
+        }
+    };
+
+    private final String[] contentType;
+
+    UploadType(String[] contentType) {
+        this.contentType = contentType;
+    }
+
+    abstract String getUploadPath();
 }
