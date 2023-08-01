5fe815032fe6db6a8bcfc7b76064491526fbf2ea
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-08-01 15:23:54 +0400

    Prepared js-service

--- auth-service/auth-application/auth-spec/src/main/java/az/rock/spec/auth/privates/command/NetworkCommandPrivateSpec.java
+++ auth-service/auth-application/auth-spec/src/main/java/az/rock/spec/auth/privates/command/NetworkCommandPrivateSpec.java
@@ -8,7 +8,15 @@ import java.util.UUID;
 public interface NetworkCommandPrivateSpec {
     ResponseEntity<JSuccessResponse> sendRequest(UUID userID);
     ResponseEntity<JSuccessResponse> acceptRequest(UUID networkID);
+
+    /**
+     * Gelen network isteyini redd etmek isteyen userin networkID-sini gonderir
+     */
     ResponseEntity<JSuccessResponse> rejectRequest(UUID networkID);
+
+    /**
+     * Oz network isteyini silmek isteyen userin networkID-sini gonderir
+     */
     ResponseEntity<JSuccessResponse> cancelRequest(UUID networkID);
     ResponseEntity<JSuccessResponse> deleteNetwork(UUID networkID);
 }
