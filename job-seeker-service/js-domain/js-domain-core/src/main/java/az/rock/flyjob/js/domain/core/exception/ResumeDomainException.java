43977b8c960db2ae6ca1e8f6609d31bcc509f729
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-08-06 20:08:00 +0400

    js-service archt

---
+++ job-seeker-service/js-domain/js-domain-core/src/main/java/az/rock/flyjob/js/domain/core/exception/ResumeDomainException.java
@@ -0,0 +1,7 @@
+package az.rock.flyjob.js.domain.core.exception;
+
+public class ResumeDomainException extends RuntimeException{
+    public ResumeDomainException(String message) {
+        super(message);
+    }
+}
