abd2c074e970b7a8311ff0dfff9ce669c8ff0ba8
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-08-14 21:06:27 +0400

    Transactional Event Streamming

--- auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/coordinator/concretes/JobSeekerCreateEventCoordinator.java
+++ auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/coordinator/concretes/JobSeekerCreateEventCoordinator.java
@@ -3,6 +3,7 @@ package az.rock.auth.domain.presentation.coordinator.concretes;
 import az.rock.auth.domain.presentation.coordinator.abstracts.AbstractJobSeekerCreateEventCoordinator;
 import az.rock.auth.domain.presentation.ports.output.publisher.AbstractUserMessagePublisher;
 import az.rock.flyjob.auth.event.user.JobSeekerCreatedEvent;
+import az.rock.lib.event.AbstractDomainEvent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,17 +17,18 @@ public class JobSeekerCreateEventCoordinator extends AbstractJobSeekerCreateEven
     }
 
     @Override
-    public void error() {
+    protected void onError(Exception exception, JobSeekerCreatedEvent event) {
+
+    }
+
+
+    @Override
+    public void onFail(AbstractDomainEvent<?> domainEvent) {
 
     }
 
     @Override
-    public void onFail() {
-
-    }
-
-    @Override
-    public void onSuccess() {
+    public void onSuccess(AbstractDomainEvent<?> domainEvent) {
 
     }
 }
---
+++ library/rock-domain/src/main/java/az/rock/lib/event/trx/State.java
@@ -0,0 +1,5 @@
+package az.rock.lib.event.trx;
+
+public enum State {
+    ON_PROCEED, ON_ERROR, ON_SUCCESS, ON_FAIL;
+}
