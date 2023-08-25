68249b9fc7a7b8acd65d131fd3ba3d3ec43eba43
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-08-25 21:12:06 +0400

    rock - domain migrated and created payload libraries

--- library/ib-payload/ib-payload-js/src/main/java/com/intellibukcet/lib/payload/event/create/SkillCreatedEvent.java
+++ library/ib-payload/ib-payload-js/src/main/java/com/intellibukcet/lib/payload/event/create/SkillCreatedEvent.java
@@ -1,6 +1,6 @@
 package com.intellibukcet.lib.payload.event.create;
 
-import az.rock.lib.event.impl.abstracts.AbstractStartDomainEvent;
+import com.intellibukcet.lib.payload.event.abstracts.AbstractStartDomainEvent;
 import com.intellibukcet.lib.payload.payload.SkillPayload;
 
 public class SkillCreatedEvent extends AbstractStartDomainEvent<SkillPayload> {
--- library/ib-payload/ib-payload-js/src/main/java/com/intellibukcet/lib/payload/payload/SkillPayload.java
+++ library/ib-payload/ib-payload-js/src/main/java/com/intellibukcet/lib/payload/payload/SkillPayload.java
@@ -1,6 +1,5 @@
 package com.intellibukcet.lib.payload.payload;
 
-import az.rock.lib.event.payload.Payload;
 
 public class SkillPayload extends Payload {
 }
