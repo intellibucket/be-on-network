8cfac6e4de99b98ef4d30b3ed181052421bcde7b
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-03 13:08:33 +0400

    rock - domain migrated and created payload libraries

--- vacancy-command-service/vacancy-command-data-access/src/main/java/com/intellibucket/flyjob/vacancy/dataAccess/model/entity/SalaryEntity.java
+++ vacancy-command-service/vacancy-command-data-access/src/main/java/com/intellibucket/flyjob/vacancy/dataAccess/model/entity/SalaryEntity.java
@@ -1,13 +1,15 @@
 package com.intellibucket.flyjob.vacancy.dataAccess.model.entity;
 
 import az.rock.lib.domain.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import az.rock.lib.valueObject.Currency;
+import az.rock.lib.valueObject.vacancy.PayingType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -18,4 +20,22 @@ import lombok.Setter;
 public class SalaryEntity extends BaseEntity {
     @OneToOne
     private VacancyEntity vacancy;
+
+    @Column(name = "has_salary")
+    private Boolean hasSalary;
+
+    @Column(name = "currency")
+    private Currency currency;
+
+    @Column(name = "start_amount")
+    private BigDecimal startAmount;
+
+    @Column(name = "end_amount")
+    private BigDecimal endAmount;
+
+    @Column(name = "is_estimated")
+    private Boolean isEstimated;
+
+    @Enumerated(EnumType.STRING)
+    private PayingType payingType;
 }
