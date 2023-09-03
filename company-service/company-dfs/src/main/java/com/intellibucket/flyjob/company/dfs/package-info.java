56d11f3ca8e31de6b1add734594375d3f2b6d175
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-03 19:03:04 +0400

    company service

---
+++ company-service/company-dfs/src/main/java/com/intellibucket/flyjob/company/dfs/package-info.java
@@ -0,0 +1,1 @@
+package com.intellibucket.flyjob.company.dfs;
--- library/rock-domain/src/main/java/az/rock/lib/valueObject/vacancy/VacancyStatus.java
+++ library/rock-domain/src/main/java/az/rock/lib/valueObject/vacancy/VacancyStatus.java
@@ -1,5 +1,5 @@
 package az.rock.lib.valueObject.vacancy;
 
 public enum VacancyStatus {
-    ACTIVE,HIDDEN,ON_PLANNING,ENDED;
+    DRAFT,ACTIVE,HIDDEN,ON_PLANNING,ENDED;
 }
