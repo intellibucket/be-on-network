6c7918bacf92363704b67c6a3808c885bd6c20bf
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-11-13 22:44:31 +0400

    ProfileHashtag updated

---
+++ auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/ports/input/util/AbstractAccessModifierReceiver.java
@@ -0,0 +1,5 @@
+package az.rock.auth.domain.presentation.ports.input.util;
+
+public interface AbstractAccessModifierReceiver {
+
+}
---
+++ auth-service/auth-domain/auth-domain-presentation/src/main/java/az/rock/auth/domain/presentation/ports/input/util/AccessModifierReceiver.java
@@ -0,0 +1,7 @@
+package az.rock.auth.domain.presentation.ports.input.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccessModifierReceiver implements AbstractAccessModifierReceiver{
+}
