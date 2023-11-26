ff8305999563e25da0ae0c911d882df7bf2b8f09
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-11-26 21:52:32 +0400

    AuthorizationApi updated

--- authorization-service/src/main/java/com/intellibucket/ws/authorization/AuthorizationApplication.java
+++ authorization-service/src/main/java/com/intellibucket/ws/authorization/AuthorizationApplication.java
@@ -1,13 +1,13 @@
-package az.rock.flyjob.auth;
+package com.intellibucket.ws.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class AuthorizationServiceApplication {
+@EnableDiscoveryClient // This annotation is optional for Eureka discovery client
+public class AuthorizationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationServiceApplication.class, args);
+        SpringApplication.run(AuthorizationApplication.class, args);
     }
 }
---
+++ authorization-service/src/main/java/com/intellibucket/ws/authorization/application/package-info.java
@@ -0,0 +1,1 @@
+package com.intellibucket.ws.authorization.application;
