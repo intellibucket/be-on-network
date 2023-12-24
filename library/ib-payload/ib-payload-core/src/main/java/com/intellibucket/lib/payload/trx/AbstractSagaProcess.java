716a80b8e5480a5644f5dd52d24a48c19285eb2b
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-12-24 17:28:58 +0400

    Event Coordinator updated

--- library/ib-payload/ib-payload-core/src/main/java/com/intellibucket/lib/payload/trx/AbstractSagaProcess.java
+++ library/ib-payload/ib-payload-core/src/main/java/com/intellibucket/lib/payload/trx/AbstractSagaProcess.java
@@ -1,6 +1,8 @@
 package com.intellibucket.lib.payload.trx;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.intellibucket.lib.payload.event.abstracts.AbstractDomainEvent;
 import com.intellibucket.lib.payload.event.abstracts.AbstractStartDomainEvent;
 import com.intellibucket.lib.payload.event.abstracts.AbstractSuccessDomainEvent;
@@ -9,7 +11,15 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "classTypeReference")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = SagaStartedProcess.class, name = "SagaStartedProcess"),
+        @JsonSubTypes.Type(value = SagaFailedProcess.class, name = "SagaFailedProcess"),
+        @JsonSubTypes.Type(value = SagaCompletedProcess.class, name = "SagaCompletedProcess")
+})
 public abstract sealed class AbstractSagaProcess<E> implements SagaTypeReference
         permits SagaStartedProcess, SagaFailedProcess, SagaCompletedProcess {
     private UUID transactionId;
@@ -35,11 +45,15 @@ public abstract sealed class AbstractSagaProcess<E> implements SagaTypeReference
     }
 
     public static <E extends AbstractStartDomainEvent<?>> AbstractSagaProcess<E> onProceed(Enum<?> step, E event) {
-        return new SagaStartedProcess<>(UUID.randomUUID(), TrxProcessStatus.STARTED, step, event);
+        return new SagaStartedProcess<>(UUID.randomUUID(), step, event);
     }
 
     public static <E> AbstractSagaProcess<? extends E> onError(AbstractSagaProcess<E> sagaProcess) {
-        return new SagaFailedProcess<>(sagaProcess.getTransactionId(), sagaProcess.step, sagaProcess.getEvent());
+        return AbstractSagaProcess.onError(sagaProcess, List.of());
+    }
+
+    public static <E> AbstractSagaProcess<? extends E> onError(AbstractSagaProcess<E> sagaProcess, List<String> errors) {
+        return new SagaFailedProcess<>(sagaProcess.getTransactionId(), sagaProcess.step, sagaProcess.getEvent(), errors);
     }
 
     public static <E, S> AbstractSagaProcess<S> onFail(AbstractSagaProcess<E> sagaProcess, Enum<?> step, S failEvent) {
--- library/ib-payload/ib-payload-core/src/main/java/com/intellibucket/lib/payload/trx/SagaStartedProcess.java
+++ library/ib-payload/ib-payload-core/src/main/java/com/intellibucket/lib/payload/trx/SagaStartedProcess.java
@@ -3,8 +3,8 @@ package com.intellibucket.lib.payload.trx;
 import java.util.UUID;
 
 public final class SagaStartedProcess<T> extends AbstractSagaProcess<T> {
-    public SagaStartedProcess(UUID transactionId, TrxProcessStatus trxProcessStatus, Enum<?> step, T event) {
-        super(transactionId, trxProcessStatus, step, event);
+    public SagaStartedProcess(UUID transactionId, Enum<?> step, T event) {
+        super(transactionId, TrxProcessStatus.STARTED, step, event);
     }
 
 }
