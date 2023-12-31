5acd850373165aa0855482e934c173e41611faf7
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-12-31 22:03:48 +0400

    CompanyCreatedListener bug fixed

--- company-command-service/company-command-domain/company-command-domain-presentation/src/main/java/com/intellibucket/onnetwork/company/domain/presentation/command/ports/input/service/abstracts/AbstractCompanyCommandDomainPresentationService.java
+++ company-command-service/company-command-domain/company-command-domain-presentation/src/main/java/com/intellibucket/onnetwork/company/domain/presentation/command/ports/input/service/abstracts/AbstractCompanyCommandDomainPresentationService.java
@@ -3,7 +3,10 @@ package com.intellibucket.onnetwork.company.domain.presentation.command.ports.in
 import az.rock.lib.domain.id.company.CompanyID;
 import com.intellibucket.lib.payload.event.create.user.CompanyCreatedEvent;
 import com.intellibucket.lib.payload.trx.SagaStartedProcess;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public interface AbstractCompanyCommandDomainPresentationService {
     void createCompany(SagaStartedProcess<CompanyCreatedEvent> request);
 
