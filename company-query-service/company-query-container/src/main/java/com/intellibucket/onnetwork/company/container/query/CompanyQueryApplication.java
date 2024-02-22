0a3ba2a0ac2feff3581aee737b40c9426b03acd9
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-02-22 18:33:22 +0400

    company query service dependencies reloded

--- company-query-service/company-query-container/src/main/java/com/intellibucket/onnetwork/company/container/query/CompanyQueryApplication.java
+++ company-query-service/company-query-container/src/main/java/com/intellibucket/onnetwork/company/container/query/CompanyQueryApplication.java
@@ -1,20 +1,26 @@
 package com.intellibucket.onnetwork.company.container.query;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication(
         scanBasePackages = {
+                "com.intellibucket.onnetwork.company.spec.query",
                 "com.intellibucket.onnetwork.company.presentation.query",
-                "com.intellibucket.onnetwork.company.domain.presentation.command",
-                "com.intellibucket.onnetwork.company.domain.core.command",
-                "com.intellibucket.onnetwork.company.dataAccess.command",
-                "com.intellibucket.onnetwork.company.messaging.command"}
+                "com.intellibucket.onnetwork.company.dataaccess.query",
+                "com.intellibucket.onnetwork.company.query.domain.presentation",
+                "com.intellibucket.onnetwork.company.query.messaging"
+        }
 )
+@ServletComponentScan
+@EntityScan(basePackages = {"com.intellibucket.onnetwork.company.dataaccess.query"})
 public class CompanyQueryApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(CompanyQueryApplication.class, args);
     }
 }
