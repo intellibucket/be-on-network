5eccbd2e6b904c653b1441e2488f0c6c15b748fc
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-02 22:42:31 +0400

    PaymentServiceApplication

---
+++ payment-service/src/main/java/com/intellibucket/onnetwork/domain/presentation/config/payment/PayPalConfig.java
@@ -0,0 +1,37 @@
+package com.intellibucket.onnetwork.domain.presentation.config.payment;
+
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PayPalConfig {
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+        return configMap;
+    }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    }
+
+    @Bean
+    public APIContext apiContext() {
+        return new APIContext(this.clientId, this.clientSecret, this.mode);
+    }
+}
