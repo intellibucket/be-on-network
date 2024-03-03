b193e9e6858968e8c40b041b605b4d447941f6ed
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-03 10:18:44 +0400

    PaymentServiceApplication

--- library/ib-payload/ib-payload-js/src/main/java/com/intellibucket/lib/payload/event/create/SkillCreatedEvent.java
+++ library/ib-payload/ib-payload-js/src/main/java/com/intellibucket/lib/payload/event/create/SkillCreatedEvent.java
@@ -4,4 +4,11 @@ import com.intellibucket.lib.payload.event.abstracts.AbstractStartDomainEvent;
 import com.intellibucket.lib.payload.payload.SkillPayload;
 
 public class SkillCreatedEvent extends AbstractStartDomainEvent<SkillPayload> {
+    public SkillCreatedEvent(SkillPayload payload) {
+        super(payload);
+    }
+
+    public static SkillCreatedEvent of(SkillPayload payload) {
+        return new SkillCreatedEvent(payload);
+    }
 }
