c20d79a6c6a4272c6c21f572108d74c0246fb3ab
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-03 10:53:03 +0400

    js-updated

---
+++ job-seeker-service/js-domain/js-domain-core/src/main/java/az/rock/flyjob/js/domain/core/exception/DuplicateSkillException.java
@@ -0,0 +1,7 @@
+package az.rock.flyjob.js.domain.core.exception;
+
+public class DuplicateSkillException extends ResumeDomainException {
+    public DuplicateSkillException() {
+        super("F0000000002");
+    }
+}
