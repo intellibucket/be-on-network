1b3cb80dbe725a58aa78e63bb49cf42ee226f2ab
Rashid-21 <resid328@gmail.com>
2024-03-03 22:38:12 +0400

    course_command

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/ports/input/services/command/concretes/CourseCommandDomainPresentationService.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/ports/input/services/command/concretes/CourseCommandDomainPresentationService.java
@@ -1,6 +1,7 @@
 package az.rock.flyjob.js.domain.presentation.ports.input.services.command.concretes;
 
 
+import az.rock.flyjob.js.domain.core.root.detail.CourseRoot;
 import az.rock.flyjob.js.domain.presentation.dto.request.abstracts.CreateRequest;
 import az.rock.flyjob.js.domain.presentation.dto.request.abstracts.UpdateRequest;
 import az.rock.flyjob.js.domain.presentation.dto.request.item.CourseCommandModel;
@@ -16,7 +17,9 @@ import java.util.UUID;
 @Service
 @InputPort
 @Slf4j
-public class CourseCommandDomainPresentationService implements AbstractCourseCommandDomainPresentationService{
+public class CourseCommandDomainPresentationService implements AbstractCourseCommandDomainPresentationService {
+
+
 
     @Override
     public void create(CreateRequest<CourseCommandModel> command) {
