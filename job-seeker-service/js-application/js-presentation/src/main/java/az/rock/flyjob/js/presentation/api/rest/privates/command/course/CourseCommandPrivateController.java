b1f9e8991f8e458bd3c2f7a52326ac775477cb55
KamilAliyev1 <kamilaliyev240@gmail.com>
2024-03-03 01:23:38 -0800

    course_command

---
+++ job-seeker-service/js-application/js-presentation/src/main/java/az/rock/flyjob/js/presentation/api/rest/privates/command/course/CourseCommandPrivateController.java
@@ -0,0 +1,65 @@
+package az.rock.flyjob.js.presentation.api.rest.privates.command.course;
+
+import az.rock.flyjob.js.domain.presentation.dto.request.abstracts.CreateRequest;
+import az.rock.flyjob.js.domain.presentation.dto.request.abstracts.UpdateRequest;
+import az.rock.flyjob.js.domain.presentation.dto.request.item.CourseCommandModel;
+import az.rock.flyjob.js.domain.presentation.dto.request.item.ReorderCommandModel;
+import az.rock.flyjob.js.domain.presentation.ports.input.services.command.abstracts.AbstractCourseCommandDomainPresentationService;
+import az.rock.flyjob.js.spec.privates.command.resume.detail.CourseCommandPrivateSpec;
+import az.rock.lib.jresponse.response.success.JSuccessResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/js/1.0/private/command/course", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CourseCommandPrivateController implements CourseCommandPrivateSpec {
+    //TODO requestlerin url-lerin duzelt
+    private final AbstractCourseCommandDomainPresentationService courseCommandDomainPresentationService;
+
+    public CourseCommandPrivateController(AbstractCourseCommandDomainPresentationService courseCommandDomainPresentationService) {
+        this.courseCommandDomainPresentationService = courseCommandDomainPresentationService;
+    }
+
+    @PostMapping
+    @Override
+    public ResponseEntity<JSuccessResponse> create(CreateRequest<CourseCommandModel> request) {
+        this.courseCommandDomainPresentationService.create(request);
+        return ResponseEntity.ok(new JSuccessResponse());
+    }
+
+    @PutMapping
+    @Override
+    public ResponseEntity<JSuccessResponse> update(UpdateRequest<CourseCommandModel> request) {
+        this.courseCommandDomainPresentationService.update(request);
+        return ResponseEntity.ok(new JSuccessResponse());
+    }
+
+    @DeleteMapping
+    @Override
+    public ResponseEntity<JSuccessResponse> delete(UUID courseId) {
+        this.courseCommandDomainPresentationService.delete(courseId);
+        return ResponseEntity.ok(new JSuccessResponse());
+    }
+
+    @PutMapping("/reorder")
+    @Override
+    public ResponseEntity<JSuccessResponse> reorder(ReorderCommandModel request) {
+        this.courseCommandDomainPresentationService.reorder(request);
+        return ResponseEntity.ok(new JSuccessResponse());
+    }
+
+    @PostMapping("/{courseId}/certificates")
+    @Override
+    public ResponseEntity<JSuccessResponse> uploadCertificate(@PathVariable UUID courseId,@RequestBody MultipartFile file) {
+        //TODO MultipartFileWrapperMapper class-ni static e cevirmek? or yene ele bir compananent yazmaq?InputSourceStream?
+        //this.courseCommandDomainPresentationService.uploadCertificate(courseId,file);
+        return ResponseEntity.ok(new JSuccessResponse());
+    }
+}
---
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/ports/input/services/command/concretes/CourseCommandDomainPresentationService.java
@@ -0,0 +1,45 @@
+package az.rock.flyjob.js.domain.presentation.ports.input.services.command.concretes;
+
+
+import az.rock.flyjob.js.domain.presentation.dto.request.abstracts.CreateRequest;
+import az.rock.flyjob.js.domain.presentation.dto.request.abstracts.UpdateRequest;
+import az.rock.flyjob.js.domain.presentation.dto.request.item.CourseCommandModel;
+import az.rock.flyjob.js.domain.presentation.dto.request.item.ReorderCommandModel;
+import az.rock.flyjob.js.domain.presentation.ports.input.services.command.abstracts.AbstractCourseCommandDomainPresentationService;
+import az.rock.lib.annotation.InputPort;
+import az.rock.lib.valueObject.MultipartFileWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@InputPort
+@Slf4j
+public class CourseCommandDomainPresentationService implements AbstractCourseCommandDomainPresentationService{
+
+    @Override
+    public void create(CreateRequest<CourseCommandModel> command) {
+
+    }
+
+    @Override
+    public void update(UpdateRequest<CourseCommandModel> command) {
+
+    }
+
+    @Override
+    public void delete(UUID courseId) {
+
+    }
+
+    @Override
+    public void reorder(ReorderCommandModel request) {
+
+    }
+
+    @Override
+    public void uploadCertificate(UUID courseId, MultipartFileWrapper file) {
+
+    }
+}
