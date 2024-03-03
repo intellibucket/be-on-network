f414b27f9a57be2ef85b50b3b25eafd3d416ebac
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-03 09:52:33 +0400

    PaymentServiceApplication

---
+++ payment-service/src/main/java/com/intellibucket/onnetwork/domain/core/root/FeatureBillingRoot.java
@@ -0,0 +1,7 @@
+package com.intellibucket.onnetwork.domain.core.root;
+
+import az.rock.lib.domain.AggregateRoot;
+import az.rock.lib.domain.id.payment.FeatureBillingID;
+
+public class FeatureBillingRoot extends AggregateRoot<FeatureBillingID> {
+}
