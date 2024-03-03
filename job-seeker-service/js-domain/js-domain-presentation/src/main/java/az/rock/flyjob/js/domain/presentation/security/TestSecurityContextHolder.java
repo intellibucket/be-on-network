e68dd3acb64eb00f42ebbe4b26eec4f8a664e1d1
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-03 10:11:36 +0400

    PaymentServiceApplication

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/security/TestSecurityContextHolder.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/security/TestSecurityContextHolder.java
@@ -12,9 +12,12 @@ import java.util.UUID;
 
 @Profile({"local", "develop"})
 @Component
-public class TestSecurityContextHolder implements AbstractSecurityContextHolder{
+public class TestSecurityContextHolder implements AbstractSecurityContextHolder {
     @Value("${test.values.user.uuid}")
     private String testUserUUID;
+
+    @Value("${test.values.resume.uuid}")
+    private String testResumeUUID;
 
     @Value("${test.values.user.language:EN}")
     private String testLanguage;
@@ -27,6 +30,7 @@ public class TestSecurityContextHolder implements AbstractSecurityContextHolder{
 
     @Value("${test.values.user.isAnonymous:false}")
     private Boolean isAnonymous ;
+
     @Value("${test.values.user.isAuthenticated:true}")
     private Boolean isAuthenticated ;
 
@@ -37,9 +41,8 @@ public class TestSecurityContextHolder implements AbstractSecurityContextHolder{
     }
 
     @Override
-    public ResumeID resumeID() {
-        // FIXME: 08.08.23
-        return null;
+    public ResumeID availableResumeID() {
+        return ResumeID.of(UUID.fromString(this.testResumeUUID));
     }
 
     @Override
