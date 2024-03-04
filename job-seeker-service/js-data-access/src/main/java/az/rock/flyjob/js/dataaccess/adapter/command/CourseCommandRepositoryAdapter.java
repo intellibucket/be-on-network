ee978b57a7390b798facb2dcd759353f2d0a0692
KamilAliyev1 <kamilaliyev240@gmail.com>
2024-03-04 07:36:45 -0800

    course_command

--- job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/adapter/command/CourseCommandRepositoryAdapter.java
+++ job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/adapter/command/CourseCommandRepositoryAdapter.java
@@ -5,11 +5,14 @@ import az.rock.flyjob.js.dataaccess.model.entity.resume.details.CourseEntity;
 import az.rock.flyjob.js.dataaccess.repository.abstracts.command.AbstractCourseCommandJPARepository;
 import az.rock.flyjob.js.domain.core.root.detail.CourseRoot;
 import az.rock.flyjob.js.domain.presentation.ports.output.repository.command.AbstractCourseCommandRepositoryAdapter;
+import az.rock.lib.valueObject.RowStatus;
 import az.rock.lib.valueObject.common.PageableRequest;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Component
 public class CourseCommandRepositoryAdapter implements AbstractCourseCommandRepositoryAdapter {
 
     private final AbstractCourseDataAccessMapper abstractCourseDataAccessMapper;
@@ -22,20 +25,16 @@ public class CourseCommandRepositoryAdapter implements AbstractCourseCommandRepo
     }
 
     @Override
-    public Optional<CourseRoot> create(CourseRoot root) {
+    public Optional<CourseRoot> merge(CourseRoot root) {
         var optional = this.abstractCourseDataAccessMapper.toEntity(root);
-        return this.abstractCourseDataAccessMapper.toRoot(
-                repository.saveAndFlush(optional.orElseThrow(()->new RuntimeException("")))
+        if(optional.isEmpty())return Optional.empty();
+        else return this.abstractCourseDataAccessMapper.toRoot(
+                repository.saveAndFlush(optional.get())
         );
     }
 
     @Override
-    public Optional<CourseRoot> update(CourseRoot root) {
-        return Optional.empty();
-    }
-
-    @Override
     public Optional<CourseRoot> delete(UUID id) {
-        return Optional.empty();
+        return this.abstractCourseDataAccessMapper.toRoot(this.repository.setRowStatusById(id,RowStatus.DELETED));
     }
 }
