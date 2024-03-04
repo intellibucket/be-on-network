1452582438170dbb0a8288a1ae60903fdfd17745
KamilAliyev1 <kamilaliyev240@gmail.com>
2024-03-03 22:04:51 -0800

    course_command

---
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/concretes/CourseCreateCommandHandler.java
@@ -0,0 +1,41 @@
+package az.rock.flyjob.js.domain.presentation.handler.concretes;
+
+import az.rock.flyjob.js.domain.presentation.dto.request.item.CourseCommandModel;
+import az.rock.flyjob.js.domain.presentation.handler.abstracts.AbstractCourseCreateCommandHandler;
+import az.rock.flyjob.js.domain.presentation.mapper.abstracts.AbstractResumeDomainMapper;
+import az.rock.flyjob.js.domain.presentation.ports.output.repository.command.AbstractResumeCommandRepositoryAdapter;
+import az.rock.lib.valueObject.MultipartFileWrapper;
+import com.intellibucket.lib.payload.event.create.CourseCreatedEvent;
+import com.intellibucket.lib.payload.event.create.CourseFileEvent;
+import com.intellibucket.lib.payload.event.delete.CourseDeleteEvent;
+import com.intellibucket.lib.payload.event.update.CourseUpdateEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CourseCreateCommandHandler implements AbstractCourseCreateCommandHandler {
+
+
+//    private final AbstractCourseDomainMapper courseDomainMapper;
+//    private final AbstractCourseCommandRepositoryAdapter courseCommandRepositoryAdapter;
+    @Override
+    public CourseCreatedEvent createCourse(CourseCommandModel command) {
+
+    }
+
+    @Override
+    public CourseUpdateEvent updateCourse(CourseCommandModel command) {
+        return null;
+    }
+
+    @Override
+    public CourseDeleteEvent deleteCourse(UUID id) {
+        return null;
+    }
+
+    @Override
+    public CourseFileEvent uploadCertificate(UUID courseId, MultipartFileWrapper file) {
+        return null;
+    }
+}
