62acaff412e8a4640a0b48762fb7700628e714f7
KamilAliyev1 <kamilaliyev240@gmail.com>
2024-03-03 23:09:32 -0800

    course_command

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/concretes/CourseCreateCommandHandler.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/concretes/CourseCreateCommandHandler.java
@@ -1,36 +1,72 @@
 package az.rock.flyjob.js.domain.presentation.handler.concretes;
 
+import az.rock.flyjob.js.domain.core.root.detail.CourseRoot;
 import az.rock.flyjob.js.domain.presentation.dto.request.item.CourseCommandModel;
 import az.rock.flyjob.js.domain.presentation.handler.abstracts.AbstractCourseCreateCommandHandler;
+import az.rock.flyjob.js.domain.presentation.mapper.abstracts.AbstractCourseDomainMapper;
+import az.rock.flyjob.js.domain.presentation.ports.output.repository.command.AbstractCourseCommandRepositoryAdapter;
+import az.rock.flyjob.js.domain.presentation.security.AbstractSecurityContextHolder;
 import az.rock.lib.valueObject.MultipartFileWrapper;
 import com.intellibucket.lib.payload.event.create.CourseCreatedEvent;
 import com.intellibucket.lib.payload.event.create.CourseFileEvent;
 import com.intellibucket.lib.payload.event.delete.CourseDeleteEvent;
 import com.intellibucket.lib.payload.event.update.CourseUpdateEvent;
+import com.intellibucket.lib.payload.payload.CourseCreatedPayload;
+import com.intellibucket.lib.payload.payload.CourseDeletedPayload;
+import com.intellibucket.lib.payload.payload.CourseUpdatedPayload;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+
+//TODO SPECIAL ERROR
 @Component
 public class CourseCreateCommandHandler implements AbstractCourseCreateCommandHandler {
+    private final AbstractCourseDomainMapper courseDomainMapper;
+    private final AbstractCourseCommandRepositoryAdapter courseCommandRepositoryAdapter;
 
+    private final AbstractSecurityContextHolder abstractSecurityContextHolder;
 
-//    private final AbstractCourseDomainMapper courseDomainMapper;
-//    private final AbstractCourseCommandRepositoryAdapter courseCommandRepositoryAdapter;
+    public CourseCreateCommandHandler(AbstractCourseDomainMapper courseDomainMapper, AbstractCourseCommandRepositoryAdapter courseCommandRepositoryAdapter, AbstractSecurityContextHolder abstractSecurityContextHolder) {
+        this.courseDomainMapper = courseDomainMapper;
+        this.courseCommandRepositoryAdapter = courseCommandRepositoryAdapter;
+        this.abstractSecurityContextHolder = abstractSecurityContextHolder;
+    }
 
     @Override
     public CourseCreatedEvent createCourse(CourseCommandModel command) {
-        return null;
+        var courseRoot = this.courseDomainMapper.toRoot(command);
+        var optionalCourseRoot = this.courseCommandRepositoryAdapter.create(courseRoot);
+
+        return CourseCreatedEvent.of(
+                CourseCreatedPayload.of(
+                    optionalCourseRoot.orElseThrow(()->new RuntimeException()).getRootID().getAbsoluteID()
+                )
+        );
+
     }
 
     @Override
     public CourseUpdateEvent updateCourse(CourseCommandModel command) {
-        return null;
+        var courseRoot = this.courseDomainMapper.toRoot(command);
+        var optionalCourseRoot = this.courseCommandRepositoryAdapter.update(courseRoot);
+
+        return CourseUpdateEvent.of(
+                CourseUpdatedPayload.of(
+                        optionalCourseRoot.orElseThrow(()->new RuntimeException()).getRootID().getAbsoluteID()
+                )
+        );
     }
 
     @Override
     public CourseDeleteEvent deleteCourse(UUID id) {
-        return null;
+        var optionalCourseRoot = this.courseCommandRepositoryAdapter.delete(id);
+
+        return CourseDeleteEvent.of(
+                CourseDeletedPayload.of(
+                        optionalCourseRoot.orElseThrow(()->new RuntimeException()).getRootID().getAbsoluteID()
+                )
+        );
     }
 
     @Override
--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/mapper/abstracts/AbstractCourseDomainMapper.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/mapper/abstracts/AbstractCourseDomainMapper.java
@@ -2,12 +2,15 @@ package az.rock.flyjob.js.domain.presentation.mapper.abstracts;
 
 
 import az.rock.flyjob.js.domain.core.root.detail.CourseRoot;
+import az.rock.flyjob.js.domain.presentation.dto.request.item.CourseCommandModel;
 import az.rock.lib.domain.id.auth.UserID;
 import com.intellibucket.lib.payload.payload.CourseFilePayload;
+import org.springframework.stereotype.Component;
+
 
 public interface AbstractCourseDomainMapper {
 
-    CourseRoot toRoot(UserID userID);
+    CourseRoot toRoot(CourseCommandModel command);
 
     CourseRoot filePayload(UserID userID, CourseFilePayload courseFilePayload);
 }
