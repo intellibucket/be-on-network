3ddbdad65aee8417b28ea2db5d239d45e1b812c3
metin <matinga@code.edu.az>
2024-03-06 14:47:19 +0400

    ContactPayload has been finished

--- library/ib-payload/ib-payload-js/src/main/java/com/intellibucket/lib/payload/payload/ContactPayload.java
+++ library/ib-payload/ib-payload-js/src/main/java/com/intellibucket/lib/payload/payload/ContactPayload.java
@@ -1,9 +1,55 @@
 package com.intellibucket.lib.payload.payload;
 
+import java.math.BigInteger;
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 public class ContactPayload {
     private UUID resumeId;
+    private Integer orderNumber;
+    private String data;
+
+    public ContactPayload(UUID resumeId, Integer orderNumber, String data) {
+        this.resumeId = resumeId;
+        this.orderNumber = orderNumber;
+        this.data = data;
+    }
+
+    private ContactPayload(Builder builder){
+        resumeId=builder.resumeId;
+        orderNumber= builder.orderNumber;
+        data= builder.data;
 
 
+    }
+
+    public static final class Builder {
+        private UUID resumeId;
+        private Integer orderNumber;
+
+        private String data;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder id(UUID val) {
+            resumeId = val;
+            return this;
+        }
+
+        public Builder orderNumber(Integer val){
+            orderNumber=val;
+            return this;
+        }
+
+        public Builder data(String val){
+            data=val;
+            return this;
+        }
+    }
 }
+
