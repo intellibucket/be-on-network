329eb9fbe69457104b3481c8f313ae07e61b425f
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-06 10:53:48 +0400

    GJpaRepository prepared

---
+++ job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/repository/abstracts/GJpaRepository.java
@@ -0,0 +1,38 @@
+package az.rock.flyjob.js.dataaccess.repository.abstracts;
+
+import az.rock.lib.domain.BaseEntity;
+import az.rock.lib.valueObject.RowStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GJpaRepository<E extends BaseEntity, ID> extends JpaRepository<E, ID> {
+
+    default void active(ID id) {
+        var optionalEntity = this.findById(id);
+        optionalEntity.ifPresent(BaseEntity::active);
+    }
+
+
+    default void deleteBySystem(ID id) {
+        var optionalEntity = this.findById(id);
+        optionalEntity.ifPresent((entity) -> {
+            entity.inActive();
+            this.save(entity);
+        });
+    }
+
+    default void deleteByUser(ID id) {
+        var optionalEntity = this.findById(id);
+        optionalEntity.ifPresent((entity) -> {
+            entity.delete();
+            this.save(entity);
+        });
+    }
+
+    default void rollback(ID id) {
+        var optionalEntity = this.findById(id);
+        optionalEntity.ifPresent((entity) -> {
+            entity.setActive(RowStatus.ROLLBACK);
+            this.save(entity);
+        });
+    }
+}
