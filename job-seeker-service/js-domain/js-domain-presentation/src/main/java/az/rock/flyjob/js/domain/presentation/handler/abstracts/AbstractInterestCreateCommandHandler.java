f57bdf09cdf7c1a82169ea2f5dfd89fffde25087
royalhsyn <royal.huseynov@bsu.edu.az>
2024-03-09 12:40:03 -0800

    handler updated

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/abstracts/AbstractInterestCreateCommandHandler.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/abstracts/AbstractInterestCreateCommandHandler.java
@@ -1,9 +1,7 @@
 package az.rock.flyjob.js.domain.presentation.handler.abstracts;
 
 import az.rock.flyjob.js.domain.presentation.dto.request.item.InterestCommandModel;
-import com.intellibucket.lib.payload.event.abstracts.AbstractSuccessDomainEvent;
 import com.intellibucket.lib.payload.event.create.InterestCreateEvent;
-import com.intellibucket.lib.payload.payload.InterestCreatePayload;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
