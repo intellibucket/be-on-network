c39285f0d57ba3b84900d4ca4ac9bfbb84727bfd
KhayyamAlizade <khayyam.a@div.edu.az>
2024-03-11 01:26:58 +0400

    interest command updated

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/concretes/InterestCreateCommandHandler.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/handler/concretes/InterestCreateCommandHandler.java
@@ -1,5 +1,6 @@
 package az.rock.flyjob.js.domain.presentation.handler.concretes;
 
+import az.rock.flyjob.js.domain.core.exception.InterestNameIsExist;
 import az.rock.flyjob.js.domain.core.exception.InterestNotFound;
 import az.rock.flyjob.js.domain.core.root.detail.InterestRoot;
 import az.rock.flyjob.js.domain.core.service.abstracts.AbstractInterestDomainService;
@@ -8,22 +9,23 @@ import az.rock.flyjob.js.domain.presentation.dto.request.item.InterestCommandMod
 import az.rock.flyjob.js.domain.presentation.dto.request.item.ReorderCommandModel;
 import az.rock.flyjob.js.domain.presentation.handler.abstracts.AbstractInterestCreateCommandHandler;
 import az.rock.flyjob.js.domain.presentation.mapper.abstracts.AbstractInterestDomainMapper;
-import az.rock.flyjob.js.domain.presentation.ports.input.services.command.abstracts.AbstractInterestCommandDomainPresentationService;
 import az.rock.flyjob.js.domain.presentation.ports.output.repository.command.AbstractInterestCommandRepositoryAdapter;
 import az.rock.flyjob.js.domain.presentation.ports.output.repository.query.AbstractInterestQueryRepositoryAdapter;
 import az.rock.flyjob.js.domain.presentation.security.AbstractSecurityContextHolder;
+import az.rock.lib.domain.AggregateRoot;
+import az.rock.lib.domain.RootID;
 import az.rock.lib.domain.id.js.InterestID;
-import az.rock.lib.domain.id.js.ResumeID;
 import com.intellibucket.lib.payload.event.create.InterestCreateEvent;
 import com.intellibucket.lib.payload.event.update.InterestDeleteEvent;
 import com.intellibucket.lib.payload.event.update.InterestReorderEvent;
 import com.intellibucket.lib.payload.event.update.InterestUpdateEvent;
+import com.intellibucket.lib.payload.payload.InterestCreatePayload;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Component
 public class InterestCreateCommandHandler implements AbstractInterestCreateCommandHandler {
@@ -51,26 +53,17 @@ public class InterestCreateCommandHandler implements AbstractInterestCreateComma
     }
 
 
-    private InterestCreatePayload toPayload(InterestRoot interestRoot) {
-        return InterestCreatePayload.Builder
-                .builder()
-                .resumeId(interestRoot.getResume().getAbsoluteID())
-                .interestId(interestRoot.getRootID().getAbsoluteID())
-                .isHobby(interestRoot.getHobby())
-                .name(interestRoot.getName())
-                .description(interestRoot.getDescription())
-                .orderNumber(interestRoot.getOrderNumber())
-                .accessModifier(interestRoot.getAccessModifier())
-                .build();
-    }
-
     @Override
-    public InterestCreateEvent add(InterestCommandModel interestCommandModel) {
+    public InterestCreateEvent add(InterestCommandModel interestCommandModel) throws InterestNameIsExist {
         var currentResume = this.securityContextHolder.availableResumeID();
         var savedInterest = this.interestQueryRepositoryAdapter.findAllByPID(currentResume);
-
-        return null;
+        var interestRoot = this.interestDomainMapper.toNewRoot(currentResume, interestCommandModel);
+        var validatedInterest = this.domainService.validateInterestName(savedInterest, interestRoot);
+        var optionalInterest = this.interestCommandRepositoryAdapter.create(validatedInterest);
+        var interestPayload = this.toPayload(optionalInterest.get());
+        return InterestCreateEvent.of(interestPayload);
     }
+
 
     @Override
     public InterestUpdateEvent update(UpdateRequest<InterestCommandModel> interestCommandModelUpdateRequest) throws Exception {
@@ -88,14 +81,7 @@ public class InterestCreateCommandHandler implements AbstractInterestCreateComma
             this.interestCommandRepositoryAdapter.update(interestRoot);
             return InterestUpdateEvent.of(interestRoot);
         } else throw new InterestNotFound("Interest not Found");
-    public InterestCreateEvent add(InterestCommandModel interestCommandModel) throws InterestNameIsExist {
-        var currentResume=this.securityContextHolder.availableResumeID();
-        var savedInterest=this.interestQueryRepositoryAdapter.findAllByPID(currentResume);
-        var interestRoot=this.interestDomainMapper.toNewRoot(currentResume,interestCommandModel);
-        var validatedInterest=this.domainService.validateInterestName(savedInterest,interestRoot);
-        var optionalInterest=this.interestCommandRepositoryAdapter.create(validatedInterest);
-        var interestPayload=this.toPayload(optionalInterest.get());
-        return InterestCreateEvent.of(interestPayload);
+
     }
 
     @Override
@@ -114,23 +100,51 @@ public class InterestCreateCommandHandler implements AbstractInterestCreateComma
     public InterestReorderEvent reorder(ReorderCommandModel request) throws InterestNotFound {
         var resumeID = this.securityContextHolder.availableResumeID();
         var allInterests = this.interestQueryRepositoryAdapter.findAllByPID(resumeID);
-        var targetInterest = allInterests.stream().filter(item -> item.getRootID().getAbsoluteID()
+        var targetInterest = allInterests.stream()
+                .filter(item -> item.getRootID().getAbsoluteID()
                         .equals(request.getTargetId()))
                 .findFirst();
         if (targetInterest.isPresent()) {
             var targetRoot = targetInterest.get();
-            var targetRootOrderNumber = targetRoot.getOrderNumber();
-            var mainRoot = allInterests.stream().filter(item -> item.getOrderNumber()
-                            .equals(request.getOrderNumber()))
-                    .findFirst()
-                    .orElseThrow(() -> new InterestNotFound("Interest not found"));
-            var changedTargetRoot = targetRoot.changeOrderNumber(mainRoot.getOrderNumber());
-            var changedMainRoot = mainRoot.changeOrderNumber(targetRootOrderNumber);
-            this.interestCommandRepositoryAdapter.update(changedTargetRoot);
-            this.interestCommandRepositoryAdapter.update(changedMainRoot);
-            return InterestReorderEvent.of(changedMainRoot.getRootID().getAbsoluteID());
+            var targetNewOrderNumber = request.getOrderNumber();
+            var targetOldOrderNumber = targetRoot.getOrderNumber();
 
+            targetRoot.changeOrderNumber(targetNewOrderNumber);
+            this.interestCommandRepositoryAdapter.update(targetRoot);
+
+
+            allInterests.stream()
+                    .filter(interest -> !interest.getRootID().getAbsoluteID().equals(request.getTargetId()))
+                    .forEach(interest -> {
+                        var currentOrderNumber = interest.getOrderNumber();
+
+                        if (targetOldOrderNumber < targetNewOrderNumber && currentOrderNumber >= targetOldOrderNumber && currentOrderNumber <= targetNewOrderNumber) {
+                            interest.changeOrderNumber(currentOrderNumber - 1);
+                            this.interestCommandRepositoryAdapter.update(interest);
+                        } else if (targetOldOrderNumber > targetNewOrderNumber && currentOrderNumber >= targetNewOrderNumber && currentOrderNumber <= targetOldOrderNumber) {
+                            interest.changeOrderNumber(currentOrderNumber + 1);
+                            this.interestCommandRepositoryAdapter.update(interest);
+                        }
+                    });
+            return InterestReorderEvent.of(targetRoot.getRootID().getAbsoluteID());
         } else throw new InterestNotFound(" Target Interest not found");
+
+    }
+
+
+    private InterestCreatePayload toPayload(InterestRoot interestRoot) {
+        return InterestCreatePayload.Builder
+                .builder()
+                .resumeId(interestRoot.getResume().getAbsoluteID())
+                .interestId(interestRoot.getRootID().getAbsoluteID())
+                .isHobby(interestRoot.getHobby())
+                .name(interestRoot.getName())
+                .description(interestRoot.getDescription())
+                .orderNumber(interestRoot.getOrderNumber())
+                .accessModifier(interestRoot.getAccessModifier())
+                .build();
     }
 
 }
+
+
