642313799801957c3dfc20d4da32a136f20f908d
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-15 22:40:01 +0400

    mybatis -generator

--- job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/model/batis/model/CourseCompose.java
+++ job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/model/batis/model/CourseCompose.java
@@ -1,9 +1,13 @@
 package az.rock.flyjob.js.dataaccess.model.batis.model;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Data
 public class CourseCompose {
-    private Object uuid;
+    private UUID uuid;
 
     private Long version;
 
@@ -15,7 +19,7 @@ public class CourseCompose {
 
     private Date modificationDate;
 
-    private Object resumeUuid;
+    private UUID resumeUuid;
 
     private String accessModifier;
 
@@ -41,155 +45,4 @@ public class CourseCompose {
 
     private String verificationAddress;
 
-    public Object getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(Object uuid) {
-        this.uuid = uuid;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
-    }
-
-    public String getProcessStatus() {
-        return processStatus;
-    }
-
-    public void setProcessStatus(String processStatus) {
-        this.processStatus = processStatus;
-    }
-
-    public String getRowStatus() {
-        return rowStatus;
-    }
-
-    public void setRowStatus(String rowStatus) {
-        this.rowStatus = rowStatus;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getModificationDate() {
-        return modificationDate;
-    }
-
-    public void setModificationDate(Date modificationDate) {
-        this.modificationDate = modificationDate;
-    }
-
-    public Object getResumeUuid() {
-        return resumeUuid;
-    }
-
-    public void setResumeUuid(Object resumeUuid) {
-        this.resumeUuid = resumeUuid;
-    }
-
-    public String getAccessModifier() {
-        return accessModifier;
-    }
-
-    public void setAccessModifier(String accessModifier) {
-        this.accessModifier = accessModifier;
-    }
-
-    public Integer getOrderNumber() {
-        return orderNumber;
-    }
-
-    public void setOrderNumber(Integer orderNumber) {
-        this.orderNumber = orderNumber;
-    }
-
-    public String getCourseTitle() {
-        return courseTitle;
-    }
-
-    public void setCourseTitle(String courseTitle) {
-        this.courseTitle = courseTitle;
-    }
-
-    public String getInstitution() {
-        return institution;
-    }
-
-    public void setInstitution(String institution) {
-        this.institution = institution;
-    }
-
-    public Boolean getIsOnline() {
-        return isOnline;
-    }
-
-    public void setIsOnline(Boolean isOnline) {
-        this.isOnline = isOnline;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getIsContinue() {
-        return isContinue;
-    }
-
-    public void setIsContinue(String isContinue) {
-        this.isContinue = isContinue;
-    }
-
-    public String getCertificateFilePath() {
-        return certificateFilePath;
-    }
-
-    public void setCertificateFilePath(String certificateFilePath) {
-        this.certificateFilePath = certificateFilePath;
-    }
-
-    public String getVerificationAddress() {
-        return verificationAddress;
-    }
-
-    public void setVerificationAddress(String verificationAddress) {
-        this.verificationAddress = verificationAddress;
-    }
 }
--- job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/model/batis/model/EducationCompose.java
+++ job-seeker-service/js-data-access/src/main/java/az/rock/flyjob/js/dataaccess/model/batis/model/EducationCompose.java
@@ -1,9 +1,13 @@
 package az.rock.flyjob.js.dataaccess.model.batis.model;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Data
 public class EducationCompose {
-    private Object uuid;
+    private UUID uuid;
 
     private Long version;
 
@@ -15,7 +19,7 @@ public class EducationCompose {
 
     private Date modificationDate;
 
-    private Object resumeUuid;
+    private UUID resumeUuid;
 
     private String accessModifier;
 
@@ -39,147 +43,4 @@ public class EducationCompose {
 
     private String description;
 
-    public Object getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(Object uuid) {
-        this.uuid = uuid;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
-    }
-
-    public String getProcessStatus() {
-        return processStatus;
-    }
-
-    public void setProcessStatus(String processStatus) {
-        this.processStatus = processStatus;
-    }
-
-    public String getRowStatus() {
-        return rowStatus;
-    }
-
-    public void setRowStatus(String rowStatus) {
-        this.rowStatus = rowStatus;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getModificationDate() {
-        return modificationDate;
-    }
-
-    public void setModificationDate(Date modificationDate) {
-        this.modificationDate = modificationDate;
-    }
-
-    public Object getResumeUuid() {
-        return resumeUuid;
-    }
-
-    public void setResumeUuid(Object resumeUuid) {
-        this.resumeUuid = resumeUuid;
-    }
-
-    public String getAccessModifier() {
-        return accessModifier;
-    }
-
-    public void setAccessModifier(String accessModifier) {
-        this.accessModifier = accessModifier;
-    }
-
-    public Integer getOrderNumber() {
-        return orderNumber;
-    }
-
-    public void setOrderNumber(Integer orderNumber) {
-        this.orderNumber = orderNumber;
-    }
-
-    public String getDegree() {
-        return degree;
-    }
-
-    public void setDegree(String degree) {
-        this.degree = degree;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public Object getEstablishmentUuid() {
-        return establishmentUuid;
-    }
-
-    public void setEstablishmentUuid(Object establishmentUuid) {
-        this.establishmentUuid = establishmentUuid;
-    }
-
-    public String getEstablishmentName() {
-        return establishmentName;
-    }
-
-    public void setEstablishmentName(String establishmentName) {
-        this.establishmentName = establishmentName;
-    }
-
-    public Object getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(Object cityId) {
-        this.cityId = cityId;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
