e3f1b238f39307b1395ca1769112c8d828a22a14
KhayyamAlizade <khayyam.a@div.edu.az>
2024-03-15 20:08:00 +0400

    query Controller apdated

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/ports/output/repository/query/AbstractInterestQueryRepositoryAdapter.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/ports/output/repository/query/AbstractInterestQueryRepositoryAdapter.java
@@ -1,7 +1,9 @@
 package az.rock.flyjob.js.domain.presentation.ports.output.repository.query;
 
+import az.rock.flyjob.js.domain.core.root.detail.InterestRoot;
 import az.rock.flyjob.js.domain.presentation.dto.response.resume.interest.AnyInterestResponseModel;
 import az.rock.flyjob.js.domain.presentation.dto.response.resume.interest.simple.SimpleAnyInterestResponseModel;
+import az.rock.lib.adapter.repository.AbstractQueryRepositoryAdapter;
 import az.rock.lib.annotation.DomainOutputPort;
 import az.rock.lib.domain.id.js.InterestID;
 import az.rock.lib.domain.id.js.ResumeID;
