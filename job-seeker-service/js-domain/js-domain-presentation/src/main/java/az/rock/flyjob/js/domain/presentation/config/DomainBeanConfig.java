ffbc5ccb407950362b2947ae0b6940e96e1b9e7c
metin <matinga@code.edu.az>
2024-03-18 11:51:00 +0400

    Reorder finished but not checked yet

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/config/DomainBeanConfig.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/config/DomainBeanConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DomainBeanConfig {
 
+
     @Bean
     AbstractInterestDomainService interestDomainService() {
         return new InterestDomainService();
