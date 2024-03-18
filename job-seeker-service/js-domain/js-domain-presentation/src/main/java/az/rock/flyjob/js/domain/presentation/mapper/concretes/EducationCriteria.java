3c2624dd0da68388218f708b45647db460b44992
huseynhasanov07 <hasanov.huseyn07@gmail.com>
2024-03-18 12:13:44 +0400

    EducationBatisRepository

--- job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/mapper/concretes/EducationCriteria.java
+++ job-seeker-service/js-domain/js-domain-presentation/src/main/java/az/rock/flyjob/js/domain/presentation/mapper/concretes/EducationCriteria.java
@@ -1,13 +1,141 @@
 package az.rock.flyjob.js.domain.presentation.mapper.concretes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EducationCriteria {
 
-    public EducationCriteria andRowStatusEqualTo(String value) {
-        addCriterion("row_status =", value, "rowStatus");
-        return (EducationCriteria) this;
+    protected List<Criterion> criteria;
+
+    protected EducationCriteria() {
+        super();
+        criteria = new ArrayList<>();
     }
-    public EducationCriteria andResumeUuidEqualTo(Object value) {
-        addCriterion("resume_uuid =", value, "resumeUuid");
+
+    public boolean isValid() {
+        return criteria.size() > 0;
+    }
+
+    public List<Criterion> getAllCriteria() {
+        return criteria;
+    }
+
+    public List<Criterion> getCriteria() {
+        return criteria;
+    }
+
+    protected void addCriterion(String condition) {
+        if (condition == null) {
+            throw new RuntimeException("Value for condition cannot be null");
+        }
+        criteria.add(new Criterion(condition));
+    }
+
+    protected void addCriterion(String condition, Object value, String property) {
+        if (value == null) {
+            throw new RuntimeException("Value for " + property + " cannot be null");
+        }
+        criteria.add(new Criterion(condition, value));
+    }
+
+    protected void addCriterion(String condition, Object value1, Object value2, String property) {
+        if (value1 == null || value2 == null) {
+            throw new RuntimeException("Between values for " + property + " cannot be null");
+        }
+        criteria.add(new Criterion(condition, value1, value2));
+    }
+
+    public EducationCriteria andUuidEqualTo(Object value) {
+        addCriterion("uuid =", value, "uuid");
         return (EducationCriteria) this;
     }
 
+
+    public static class Criterion {
+        private String condition;
+
+        private Object value;
+
+        private Object secondValue;
+
+        private boolean noValue;
+
+        private boolean singleValue;
+
+        private boolean betweenValue;
+
+        private boolean listValue;
+
+        private String typeHandler;
+
+        protected Criterion(String condition) {
+            super();
+            this.condition = condition;
+            this.typeHandler = null;
+            this.noValue = true;
+        }
+
+        protected Criterion(String condition, Object value, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.typeHandler = typeHandler;
+            if (value instanceof List<?>) {
+                this.listValue = true;
+            } else {
+                this.singleValue = true;
+            }
+        }
+
+        protected Criterion(String condition, Object value) {
+            this(condition, value, null);
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue, String typeHandler) {
+            super();
+            this.condition = condition;
+            this.value = value;
+            this.secondValue = secondValue;
+            this.typeHandler = typeHandler;
+            this.betweenValue = true;
+        }
+
+        protected Criterion(String condition, Object value, Object secondValue) {
+            this(condition, value, secondValue, null);
+        }
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public Object getSecondValue() {
+            return secondValue;
+        }
+
+        public boolean isNoValue() {
+            return noValue;
+        }
+
+        public boolean isSingleValue() {
+            return singleValue;
+        }
+
+        public boolean isBetweenValue() {
+            return betweenValue;
+        }
+
+        public boolean isListValue() {
+            return listValue;
+        }
+
+        public String getTypeHandler() {
+            return typeHandler;
+        }
+
+
+    }
 }
