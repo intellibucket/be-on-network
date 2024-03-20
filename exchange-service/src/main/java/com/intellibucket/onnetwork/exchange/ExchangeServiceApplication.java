196066282779818d855a2f14eb67140e696a64c6
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-20 15:04:57 +0400

    ExchangeServiceApplication.java prepared

--- exchange-service/src/main/java/com/intellibucket/onnetwork/exchange/ExchangeServiceApplication.java
+++ exchange-service/src/main/java/com/intellibucket/onnetwork/exchange/ExchangeServiceApplication.java
@@ -2,12 +2,10 @@ package com.intellibucket.onnetwork.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-@ServletComponentScan
+@SpringBootApplication
 public class ExchangeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExchangeServiceApplication.class, args);
