e23a1a090b7e1cfd7df1dfa3ce6c1c32f539fe7e
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2024-03-20 14:19:43 +0400

    ExchangeServiceApplication.java prepared

---
+++ exchange-service/src/main/java/com/intellibucket/onnetwork/domain/presentation/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.intellibucket.onnetwork.domain.presentation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
